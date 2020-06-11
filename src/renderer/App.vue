24ec4e9c1b5951524e60b86bb852861743377a4f
maribor666 <spekaoleh@ukr.net>
2020-06-11 19:53:01 +0300

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'basin_management_tool',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
